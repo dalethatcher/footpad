0dc68e72d16f764655955f0f6f0f74f932b0bff6
Dale Thatcher <github@dalethatcher.com>
2011-02-22 20:52:04 +0000

    I can get in but there is not much space.

--- src/footpad/core.clj
+++ src/footpad/core.clj
@@ -8,3 +8,14 @@
 
 (defn generate-rock [width height]
   (repeatedly height #(take width (cycle [\#]))))
+
+(defn insert-stairs [row width]
+  (let [x (inc (rand-int (- width 2)))
+        [left right] (split-at x row)]
+    (concat left [\>] (rest right))))
+
+(defn generate-rock-with-stairs [room width height]
+  (let [y (inc (rand-int (- height 2)))
+        [top bottom] (split-at y room)
+        to-change (first bottom)]
+    (concat top [(insert-stairs to-change width)] (rest bottom))))
